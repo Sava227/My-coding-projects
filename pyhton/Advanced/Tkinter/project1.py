664b64713de0edadcc3637478bf2b308c214ff69
Savelii Artiushenko <artuishenks@gmail.com>
2026-03-10 21:06:17 +0500

    projct1.py is done

--- pyhton/Advanced/Tkinter/project1.py
+++ pyhton/Advanced/Tkinter/project1.py
@@ -3,14 +3,19 @@ from tkinter import *
 window = Tk()
 window.title("Vidjets and arguments")
 window.geometry("400x250")
-frame_pack = LabelFrame(window, text="Project1", padx=10, pady=10, width=350, height=100)
-frame_pack.grid(padx=30, pady=20)
 
-label1 = Label(window, text="Hello world", relief=SUNKEN, bd=5, bg="gray59", fg="white", font="helvetica 8 bold", width=50, height=5)
-label1.grid()
+frame1 = Frame(window, width=300, height=100, bg="purple1")
+frame1.place(x=10, y=10)
+label1 = Label(frame1, text="Hello world", relief=RAISED, bd=5, bg="purple1", fg="white", font="helvetica 8 bold", width=50, height=5, justify="center")
+label1.place(x=10, y=10)
 
-button1 = Button(frame_pack, text="Button 1")
+frame2 = Frame(window, width=300, height=100, bg="purple1")
+frame2.place(x=10, y=150)
 
-button1.place(x=10, y=6)
+button1 = Button(frame2, text="Button 1", activebackground="blue", activeforeground="yellow", )
+button2 = Button(frame2, text="Button 2", activeforeground="red", activebackground="green")
+
+button1.place(x=40, y=30)
+button2.place(x=170, y=30)
 
 window.mainloop()
