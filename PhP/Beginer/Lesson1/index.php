c1c685477f7ca4267eeb1d749268694ddbe56845
Savelii Artiushenko <artuishenks@gmail.com>
2026-04-07 17:03:39 +0500

    php

---
+++ PhP/Beginer/Lesson1/index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Lesson 1</title>
+</head>
+<body>
+    <h1>
+        <?php
+        $greeting = "Hello";
+
+        echo "$greeting everyone!"; //do not use single quotes '...' to execute the output with the variable
+        ?>
+    </h1>
+</body>
+</html>
